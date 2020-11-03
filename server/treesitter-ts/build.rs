9a7aade33a7051d6903479c8088aaaf5acd2a2d1
IWANABETHATGUY <974153916@qq.com>
2020-11-04 01:40:33 +0800

    feat: 🎸 meaningless commit

--- server/treesitter-ts/build.rs
+++ server/treesitter-ts/build.rs
@@ -2,6 +2,7 @@ use std::path::PathBuf;
 fn main() {
     let dir: PathBuf = ["tree-sitter-typescript", "typescript", "src"].iter().collect();
 
+
     cc::Build::new()
         .include(&dir)
         .file(dir.join("parser.c"))
