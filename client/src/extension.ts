0d282a539e5d21d363c097370ebf2a48b1d07aab
IWANABETHATGUY <974153916@qq.com>
2020-11-04 00:02:04 +0800

    feat: 🎸 make it runnable

---
+++ client/src/extension.ts
@@ -0,0 +1,66 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as path from "path";
+import { workspace, ExtensionContext } from "vscode";
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+  Executable
+} from "vscode-languageclient";
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+  // The server is implemented in node
+  // let serverModule = context.asAbsolutePath(
+  // 	path.join('server', 'out', 'server.js')
+  // );
+  // The debug options for the server
+  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+  // let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+
+  // E:\vscode-extension\github\server\target\debug
+  const run: Executable = {
+    command: 'E:\\vscode-extension\\github\\server\\target\\debug\\server.exe',
+  };
+  const serverOptions: ServerOptions = {
+    run,
+    debug: run,
+  };
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+
+  // Options to control the language client
+  let clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [{ scheme: "file", language: "plaintext" }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    "languageServerExample",
+    "Language Server Example",
+    serverOptions,
+    clientOptions
+  );
+
+  // Start the client. This will also launch the server
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
